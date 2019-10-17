3920b80781c66647508fb8237e6a8d5e50515c1d
Vladyslav Panchenko <vladlibero@gmail.com>
2019-10-17 22:13:09 +0300

    messages time

--- public/main.js
+++ public/main.js
@@ -8,225 +8,230 @@ const create = tag => document.createElement(tag)
 
 // Creates alert div for login and register forms
 const placeAlert = (parent, alertType, alertText) => {
-  if (parent.firstChild.tagName !== "FORM")
-    parent.removeChild(parent.firstChild)
-  let loginAlert = create("div")
-  loginAlert.classList.add(
-    "ml-auto",
-    "mr-auto",
-    "col-lg-6",
-    "alert",
-    `alert-${alertType}`
-  )
-  loginAlert.innerText = alertText
-  parent.prepend(loginAlert)
+   if (parent.firstChild.tagName !== "FORM")
+      parent.removeChild(parent.firstChild)
+   let loginAlert = create("div")
+   loginAlert.classList.add(
+      "ml-auto",
+      "mr-auto",
+      "col-lg-6",
+      "alert",
+      `alert-${alertType}`
+   )
+   loginAlert.innerText = alertText
+   parent.prepend(loginAlert)
 }
 
 const placeMessage = ({ name, message, imageMsg, id }) => {
-  //TODO message date&time
-  let messageNode = create("p")
-  messageNode.id = id
-  let userSpan = create("span")
-  userSpan.innerText = `${name}: `
-  name === nickname
-    ? (userSpan.style.color = "orange")
-    : (userSpan.style.color = "blue")
-    //if text 
-    if (message) {
-        let msg = create("span")
-        msg.innerText = message
-        messageNode.appendChild(userSpan)
-        messageNode.appendChild(msg)
-    }
-    // if image
-    else {
-        let img = create("img")
-        img.src = imageMsg
-        img.style.width = '300px'
-        messageNode.appendChild(userSpan)
-        messageNode.appendChild(img)
-    }
-  messagesContainer.appendChild(messageNode)
-  messageNode.scrollIntoView({block: "end", behavior: "smooth"})
+   let messageNode = create("p")
+   messageNode.id = id
+   let timeSpan = create("span")
+   let curDate = new Date()
+   timeSpan.innerText = `<<${curDate.toLocaleString()}>> `
+   timeSpan.style.fontWeight = "lighter"
+   timeSpan.style.fontStyle = "italic"
+   timeSpan.style.fontSize = "0.75rem"
+   timeSpan.style.color = "grey"
+   messageNode.appendChild(timeSpan)
+   let userSpan = create("span")
+   userSpan.innerText = `${name}: `
+   name === nickname
+      ? (userSpan.style.color = "orange")
+      : (userSpan.style.color = "blue")
+   messageNode.appendChild(userSpan)
+   //if text
+   if (message) {
+      let msg = create("span")
+      msg.innerText = message
+      messageNode.appendChild(msg)
+   }
+   // if image
+   else {
+      let img = create("img")
+      img.src = imageMsg
+      img.style.width = "300px"
+      messageNode.appendChild(img)
+   }
+   messagesContainer.appendChild(messageNode)
+   messageNode.scrollIntoView({ block: "end", behavior: "smooth" })
 }
 
-let readAndHash = (inputTagId) => {
-  let sha = new jsSHA("SHA-256", "TEXT")
-  sha.update(inputTagId.value)
-  return sha.getHash("HEX")
+let readAndHash = inputTagId => {
+   let sha = new jsSHA("SHA-256", "TEXT")
+   sha.update(inputTagId.value)
+   return sha.getHash("HEX")
 }
 
 //* Event Handlers
 loginForm.onsubmit = async event => {
-  event.preventDefault()
-  if (nicknameInp.value !== "" && passwordInp.value !== "") {
-    nickname = loginForm.nickname.value
-    loginForm.nickname.value = ""
-
-    let password = readAndHash(loginForm.password)
-    loginForm.password.value = ""
-    formBtn.innerText = "  Checking..."
-    let spinner = create("span")
-    spinner.classList.add("spinner-border", "spinner-border-sm")
-    formBtn.prepend(spinner)
-    let usersRequest = await fetch("/users")
-    users = await usersRequest.json()
-    //TODO check if remember me checked (keep in cookie?)
-    let userFound = users.find(
-      user => user.name === nickname && user.password === password
-    )
-    if (userFound) {
-      rememberCheck.checked 
-        ? localStorage.setItem('rememberedUser', JSON.stringify([nickname, password]))
-        : null
-      loginContainer.classList.add("d-none")
-      chatContainer.classList.remove("d-none")
-      let startListening = setInterval(async () => {
-        let historyRequest = await fetch("/messages")
-        let history = await historyRequest.json()
-        for (let msg of history)
-          if (
-            !messagesContainer.lastChild ||
-            +msg.id > +messagesContainer.lastChild.id
-          )
-            placeMessage(msg)
-      }, 1500)
-    } else {
-      let alertText = "Password incorrect or user was not found"
-      placeAlert(loginContainer, "danger", alertText)
-    }
-  } else {
-    let alertText = "Either nickname or password was not filled"
-    placeAlert(loginContainer, "warning", alertText)
-  }
-  formBtn.innerText = "Login"
+   event.preventDefault()
+   if (nicknameInp.value !== "" && passwordInp.value !== "") {
+      nickname = loginForm.nickname.value
+      loginForm.nickname.value = ""
+      let password
+      //if event initiated by user we  need to hash his password
+      if (event.isTrusted) password = readAndHash(loginForm.password)
+      //otherwise we will read it from local storage and give to input
+      //its hashed already (this is a part of "remember me" logic)
+      else password = loginForm.password.value
+      loginForm.password.value = ""
+      formBtn.innerText = "  Checking..."
+      let spinner = create("span")
+      spinner.classList.add("spinner-border", "spinner-border-sm")
+      formBtn.prepend(spinner)
+      let usersRequest = await fetch("/users")
+      users = await usersRequest.json()
+      let userFound = users.find(
+         user => user.name === nickname && user.password === password
+      )
+      if (userFound) {
+         rememberCheck.checked
+            ? localStorage.setItem(
+                 "rememberedUser",
+                 JSON.stringify([nickname, password])
+              )
+            : null
+         loginContainer.classList.add("d-none")
+         chatContainer.classList.remove("d-none")
+         let startListening = setInterval(async () => {
+            let historyRequest = await fetch("/messages")
+            let history = await historyRequest.json()
+            for (let msg of history)
+               if (
+                  !messagesContainer.lastChild ||
+                  +msg.id > +messagesContainer.lastChild.id
+               )
+                  placeMessage(msg)
+         }, 1500)
+      } else {
+         let alertText = "Password incorrect or user was not found"
+         placeAlert(loginContainer, "danger", alertText)
+      }
+   } else {
+      let alertText = "Either nickname or password was not filled"
+      placeAlert(loginContainer, "warning", alertText)
+   }
+   formBtn.innerText = "Login"
 }
 
 registerForm.onsubmit = async event => {
-  event.preventDefault()
-  if (regnameInp.value !== "" && regpassInp.value !== "") {
-    regBtn.innerText = "  Checking..."
-    let spinner = create("span")
-    spinner.classList.add("spinner-border", "spinner-border-sm")
-    regBtn.prepend(spinner)
-    let usersRequest = await fetch("/users")
-    users = await usersRequest.json()
-    let userAttemp = registerForm.nickname.value
-    let userFound = users.find(user => user.name === userAttemp)
-    if (!userFound) {
-      registerForm.nickname.value = ""
-      let password = readAndHash(registerForm.password)
-      registerForm.password.value = ""
-      let postNewUser = await fetch("/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ name: userAttemp, password: password })
-      })
-      if (postNewUser.status < 305 && postNewUser.status > 199) {
-        let alertText =
-          "Registration successful, thank you! You can go BACK and login now"
-        placeAlert(registrationContainer, "success", alertText)
+   event.preventDefault()
+   if (regnameInp.value !== "" && regpassInp.value !== "") {
+      regBtn.innerText = "  Checking..."
+      let spinner = create("span")
+      spinner.classList.add("spinner-border", "spinner-border-sm")
+      regBtn.prepend(spinner)
+      let usersRequest = await fetch("/users")
+      users = await usersRequest.json()
+      let userAttemp = registerForm.nickname.value
+      let userFound = users.find(user => user.name === userAttemp)
+      if (!userFound) {
+         registerForm.nickname.value = ""
+         let password = readAndHash(registerForm.password)
+         registerForm.password.value = ""
+         let postNewUser = await fetch("/users", {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name: userAttemp, password: password })
+         })
+         if (postNewUser.status < 305 && postNewUser.status > 199) {
+            let alertText =
+               "Registration successful, thank you! You can go BACK and login now"
+            placeAlert(registrationContainer, "success", alertText)
+         } else {
+            let alertText = "Something went wrong, please try again"
+            placeAlert(registrationContainer, "danger", alertText)
+         }
       } else {
-        let alertText = "Something went wrong, please try again"
-        placeAlert(registrationContainer, "danger", alertText)
+         let alertText = "Such user already exists. Try another name"
+         placeAlert(registrationContainer, "danger", alertText)
       }
-    } else {
-      let alertText = "Such user already exists. Try another name"
-      placeAlert(registrationContainer, "danger", alertText)
-    }
-  } else {
-    let alertText = "Either nickname or password was not filled"
-    placeAlert(registrationContainer, "warning", alertText)
-  }
-  regBtn.innerText = "Create account"
+   } else {
+      let alertText = "Either nickname or password was not filled"
+      placeAlert(registrationContainer, "warning", alertText)
+   }
+   regBtn.innerText = "Create account"
 }
 
 //TODO logout btn
 //TODO forget me chckbox
 
 createBadge.onclick = event => {
-  event.preventDefault()
-  loginContainer.classList.add("d-none")
-  registrationContainer.classList.remove("d-none")
-  if (loginContainer.firstChild.tagName !== "FORM")
-    loginContainer.removeChild(loginContainer.firstChild)
+   event.preventDefault()
+   loginContainer.classList.add("d-none")
+   registrationContainer.classList.remove("d-none")
+   if (loginContainer.firstChild.tagName !== "FORM")
+      loginContainer.removeChild(loginContainer.firstChild)
 }
 
 gobackBadge.onclick = event => {
-  event.preventDefault()
-  loginContainer.classList.remove("d-none")
-  registrationContainer.classList.add("d-none")
-  if (registrationContainer.firstChild.tagName !== "FORM")
-    registrationContainer.removeChild(registrationContainer.firstChild)
+   event.preventDefault()
+   loginContainer.classList.remove("d-none")
+   registrationContainer.classList.add("d-none")
+   if (registrationContainer.firstChild.tagName !== "FORM")
+      registrationContainer.removeChild(registrationContainer.firstChild)
 }
 
 msgForm.onsubmit = async event => {
-  event.preventDefault()
-  let { value: newMsg } = msgInp
-  msgInp.value = ""
-  if (newMsg !== "") {
-    let msgSentToServer = await fetch("/messages", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ name: nickname, message: newMsg })
-    })
-    let reply = await msgSentToServer.json()
-    placeMessage(reply)
-  }
+   event.preventDefault()
+   let { value: newMsg } = msgInp
+   msgInp.value = ""
+   if (newMsg !== "") {
+      let msgSentToServer = await fetch("/messages", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify({ name: nickname, message: newMsg })
+      })
+      let reply = await msgSentToServer.json()
+      placeMessage(reply)
+   }
 }
 
 sendImgBtn.onclick = async () => {
-        //*reading image
-  let { files: uploadedFiles } = fileInput
-  fileInput.files[0] = null
-  fileLabel.innerText = "Choose file..."
-  if (uploadedFiles.length) {
-    let fileReader = new FileReader()
-    fileReader.onload = async event => {
-        let baseData = event.target.result
-        let imgSentToServer = await fetch("/messages", {
+   //*reading image
+   let { files: uploadedFiles } = fileInput
+   fileInput.files[0] = null
+   fileLabel.innerText = "Choose file..."
+   if (uploadedFiles.length) {
+      let fileReader = new FileReader()
+      fileReader.onload = async event => {
+         let baseData = event.target.result
+         let imgSentToServer = await fetch("/messages", {
             method: "POST",
             headers: {
-              "Content-Type": "application/json"
+               "Content-Type": "application/json"
             },
             body: JSON.stringify({ name: nickname, imageMsg: baseData })
-          })
-        let reply = await imgSentToServer.json()
-        placeMessage(reply)
-
-    }
-    fileReader.readAsDataURL(uploadedFiles[0])
-  }
+         })
+         let reply = await imgSentToServer.json()
+         placeMessage(reply)
+      }
+      fileReader.readAsDataURL(uploadedFiles[0])
+   }
 }
 
 fileInput.onchange = () => {
-     fileLabel.innerText = fileInput.files[0].name
-    
+   fileLabel.innerText = fileInput.files[0].name
 }
-
 
 //! onchange for image input => change label and enable upload btn
 
+//this is "remember me" logic
+;(function checkLocal() {
+   let user = JSON.parse(localStorage.getItem("rememberedUser"))
+   if (user) {
+      nicknameInp.value = user[0]
+      passwordInp.value = user[1] 
+      let submit = new Event("submit")
+      loginForm.dispatchEvent(submit)
+   }
+})()
 
 //TODO private rooms, friends
-  //TODO sending pictures
-  //TODO account settings, avatar, title
-  //TODO smiles?
-  //TODO sedning files?
-  //TODO statuses (online, offline, invisible, afk)
+//TODO account settings, avatar, title
+//TODO statuses (online, offline, invisible, afk)
 //TODO system messages in chat
-
-
-(function checkLocal() {
-  let user = JSON.parse(localStorage.getItem('rememberedUser'))
-  if (user.length) {
-    nicknameInp.value = user[0]
-    passwordInp.value = user[1] // damn its hashed
-    let submit = new Event('submit')
-    loginForm.dispatchEvent(submit)
-  }
-})()
